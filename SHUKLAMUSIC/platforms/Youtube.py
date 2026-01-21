3aad1fbb7810b0468964b0470e32b937f4799a38
Shashank Shukla <147219905+itzshukla@users.noreply.github.com>
2026-01-21 12:49:46 +0530

    Update Youtube.py

--- SHUKLAMUSIC/platforms/Youtube.py
+++ SHUKLAMUSIC/platforms/Youtube.py
@@ -15,23 +15,6 @@
 import asyncio
 import os
 import re
-# ---------------------------------------------------------------
-# 🔸 Shashank YT-API Project
-# 🔹 Developed & Maintained by: Shashank Shukla (https://github.com/itzshukla)
-# 📅 Copyright © 2025 – All Rights Reserved
-#
-# 📖 License:
-# This source code is open for educational and non-commercial use ONLY.
-# You are required to retain this credit in all copies or substantial portions of this file.
-# Commercial use, redistribution, or removal of this notice is strictly prohibited
-# without prior written permission from the author.
-#
-# ❤️ Made with dedication and love by ItzShukla
-# ---------------------------------------------------------------
-
-import asyncio
-import os
-import re
 import json
 from typing import Union
 import requests
