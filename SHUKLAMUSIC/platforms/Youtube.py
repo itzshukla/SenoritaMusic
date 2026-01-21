8dfe0a271b29101c62b6c9295705b64bc4b8ce9a
Shashank Shukla <147219905+itzshukla@users.noreply.github.com>
2026-01-21 12:49:08 +0530

    Update Youtube.py

--- SHUKLAMUSIC/platforms/Youtube.py
+++ SHUKLAMUSIC/platforms/Youtube.py
@@ -38,7 +38,7 @@ import requests
 import yt_dlp
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
-from py_yt import VideosSearch
+from youtubesearchpython.__future__ import VideosSearch
 from ..utils.database import is_on_off, get_assistant
 from ..utils.formatters import time_to_seconds
 from SHUKLAMUSIC import app
